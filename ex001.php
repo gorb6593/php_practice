f704fb59919d1049327c10d32928a9d51808f7a7
haekyu <gorb6593@gmail.com>
2022-08-16 23:59:12 +0900

    test

---
+++ ex001.php
@@ -0,0 +1,15 @@
+<?php
+    $a = 1.1;
+    $b = 0.1;
+
+    if($a+$b == 1.2){
+        echo "true";
+    }else{
+        echo "false";
+    }
+    echo "<br>";
+    var_dump($a);
+
+    $a + $b == 12;
+    // 메모리에 소수 저장
+?>
